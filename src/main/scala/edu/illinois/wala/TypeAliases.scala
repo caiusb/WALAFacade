02daae68e5dcfa9f160bb337ce0e8da709461459
Caius Brindescu <brindesc@oregonstate.edu>
2016-06-02 13:07:38 -0700

    Using the SSAAbstractInvokeInstruction as the supertype
    The AST Invoke is rooted in this SSAAbstractInvoke...,
    not SSAInvokeI....
    SSAAbstractInvoke...  also a superclass of SSAInvoke...
    so everything else works fine

--- src/main/scala/edu/illinois/wala/TypeAliases.scala
+++ src/main/scala/edu/illinois/wala/TypeAliases.scala
@@ -40,7 +40,7 @@ trait TypeAliases {
   type ArrayLoadI = SSAArrayLoadInstruction
   type ArrayReferenceI = SSAArrayReferenceInstruction
 
-  type InvokeI = SSAInvokeInstruction
+  type InvokeI = SSAAbstractInvokeInstruction
 
   type MonitorI = SSAMonitorInstruction
 
